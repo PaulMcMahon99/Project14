5fefdaa294ac8359040fd0cb9cb37e28b81f3511
Paul Mcmahon <mcmahonp1@cardiff.ac.uk>
2020-05-06 19:11:26 +0100

    Initial commit

---
+++ src/sample/detailController.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class detailController {
+
+    @FXML
+    private ComboBox comboBoxDetail;
+
+    @FXML
+    private void initialize(){
+
+        comboBoxDetail.getItems().setAll("Aberporth", "Armagh", "Ballypatrick Forest", "Bradford", "Braemar", "Camborne",
+                "Cambridge", "Cambridge NIAB", "Cardiff Bute Park", "Chivenor", "Cwmystwyth", "Dunstaffnage", "Durham", "Eastbourne",
+                "Eskdalemuir", "Heathrow", "Hurn", "Lerwick", "Leuchars", "Lowestoft", "Manston", "Nairn", "Newton Rigg", "Oxford",
+                "Paisley", "Ringway", "Ross-on-Wye", "Shawbury", "Sheffield", "Southampton", "Stornoway Airport", "Sutton Bonington",
+                "Tiree", "Valley", "Waddington", "Whitby", "Wick Airport", "Yeovilton");
+
+    }
+
+}
---
+++ src/sample/reportController.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class reportController {
+
+    @FXML
+    private ComboBox comboBoxReport;
+
+    @FXML
+    private void initialize(){
+
+        comboBoxReport.getItems().setAll("Aberporth", "Armagh", "Ballypatrick Forest", "Bradford", "Braemar", "Camborne",
+                "Cambridge", "Cambridge NIAB", "Cardiff Bute Park", "Chivenor", "Cwmystwyth", "Dunstaffnage", "Durham", "Eastbourne",
+                "Eskdalemuir", "Heathrow", "Hurn", "Lerwick", "Leuchars", "Lowestoft", "Manston", "Nairn", "Newton Rigg", "Oxford",
+                "Paisley", "Ringway", "Ross-on-Wye", "Shawbury", "Sheffield", "Southampton", "Stornoway Airport", "Sutton Bonington",
+                "Tiree", "Valley", "Waddington", "Whitby", "Wick Airport", "Yeovilton");
+    }
+}
