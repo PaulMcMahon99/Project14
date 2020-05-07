65f08a87892ff85cd4d68829b52987c59549a915
Paul Mcmahon <mcmahonp1@cardiff.ac.uk>
2020-05-07 17:21:55 +0100

    Overview now works with an image to be replaced with the full view soon.  Detail next.

--- src/sample/overviewController.java
+++ src/sample/overviewController.java
@@ -1,12 +1,19 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 
 public class overviewController {
 
+    @FXML
+    private ImageView imgView;
 
-
+    @FXML
+    private void initialize(){
+        Image image = new Image("sample/img/OverviewTest.png");
+        imgView.setImage(image);
     }
 
-
+}
